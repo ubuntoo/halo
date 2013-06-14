df8bf16c32312d4df735ababe7719a8bb2ffca48
wangwei <ubunto@sina.cn>
2013-06-14 21:44:47 +0800

    Revert "halo代码提交"
    This reverts commit 039140d3953dc18409a92ab5ea863a0beb2720dd.

--- halo/Dispatch/framework/dispatcher/IDispatcher.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace framework\dispatcher;
-
-/**
- * 请求调度接口
- */
-interface IDispatcher {
-	public function distribute();
-}
--- halo/Webroot/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * web入口
- * 
- * @version
- * @author  wei.wang <ubunto@sina.cn>
- */
-require realpath(dirname(__FILE__) . DIRECTORY_SEPARATOR . '..') . DIRECTORY_SEPARATOR . 'Dispatch' . DIRECTORY_SEPARATOR . 'Halo.php';
-Halo::run(Halo::HALO_WEB);
